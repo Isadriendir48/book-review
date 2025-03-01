6d7086842bb93c2b7752b8c7aac3e2df8c96224d
Andres Olguin <andres.ed.olg.c@gmail.com>
2025-03-01 09:44:19 -0600

    Added local scope query for book title.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Book extends Model
 {
@@ -13,5 +15,10 @@ class Book extends Model
     public function reviews(): HasMany
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'like', '%' . $title . '%');
     }
 }
